1ee644fa0b7d663026fa9c41af99f87f4eabcf6a
Ruziev-Gofur <gafur_ru@bk.ru>
2022-09-29 11:43:44 +0500

    mahsulot crud kamchiliklari bor

---
+++ app/Http/Controllers/ProductController.php
@@ -0,0 +1,104 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Firm;
+use App\Models\Product;
+use Illuminate\Http\Request;
+
+class ProductController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $product=Product::all();
+
+        $products=[];
+        foreach ($products as $product){
+            $products[$product->id]=$product;
+        }
+        return view('product',[
+            'products'=>$products,
+            'productes'=>$products,
+        ]);
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+
+        $request->validate([
+            'name'=>'required'
+        ]);
+        $proruct=new Firm();
+        $proruct->name=$request['name'];
+
+        $proruct->save();
+        return redirect()->route('products.index')->with('success', 'Mahsulot muvaffaqqiyatli yaratildi');
+
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function show($id)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function edit($id)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, $id)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  int  $id
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy($id)
+    {
+        //
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -25,6 +25,7 @@ Route::middleware('auth')->group(function () {
     Route::resource('type',\App\Http\Controllers\TypeController::class);
     Route::resource('worker_gaves',\App\Http\Controllers\WorkerGaveController::class);
     Route::resource('jobs',\App\Http\Controllers\JobsController::class);
+    Route::resource('products',\App\Http\Controllers\ProductController::class);
 
 });
 
