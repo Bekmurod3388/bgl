41bd4c311cc7e04bef64c715617f57007ea78008
DiyorbekBoltayev <boltayevdiyorbek017@gmail.com>
2022-09-29 20:59:16 +0500

    header fixed

--- resources/views/header.blade.php
+++ resources/views/header.blade.php
@@ -5,12 +5,8 @@
             <div class="col-sm-6">
                 <h1 class="m-0">@yield('title')</h1>
             </div><!-- /.col -->
-            <div class="col-sm-6">
-                <ol class="breadcrumb float-sm-right">
-                    <li class="breadcrumb-item"><a href="#">Home</a></li>
-                    <li class="breadcrumb-item active">Kirim sahifasi</li>
-                </ol>
-            </div><!-- /.col -->
+
+
         </div><!-- /.row -->
     </div><!-- /.container-fluid -->
 </div>
